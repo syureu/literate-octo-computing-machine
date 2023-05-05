0432b86751a6cd5e1696d4c0067be74c4eacd844
syureu <tor01237@gmail.com>
2023-05-05 21:00:49 +0900

    Initial commit

---
+++ WyCash/src/main/java/me/syureu/wycash/WyCashApplication.java
@@ -0,0 +1,13 @@
+package me.syureu.wycash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyCashApplication.class, args);
+	}
+
+}
---
+++ WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
@@ -0,0 +1,13 @@
+package me.syureu.wycash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
