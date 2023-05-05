79ab15dfb6f569c01069c4c3b1c2cfdd8db6017b
syureu <tor01237@gmail.com>
2023-05-05 21:06:45 +0900

    다중 통화를 지원하는 보고서 만들기
    할일 목록

--- WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
+++ WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
@@ -6,8 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WyCashApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/**
+	 * 통화가 다른 두 금액을 더해서 주어진 환율에 맞게 변한 금액을 얻을 수 있어야 한다.
+	 */
 
+	/**
+	 * 어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.
+	 */
+
+	
 }
