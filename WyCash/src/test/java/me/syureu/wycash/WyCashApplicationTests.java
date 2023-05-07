00af870780d6bec5ac36cb0309deb34238484e10
syureu <tor01237@gmail.com>
2023-05-07 20:49:18 +0900

    불변을 위한 Dollar의 vo화
    책에서는 삼각측량이라는 표현을 썼는데 이를 하기 위해서는 2개이상의 예제가 있어야
    코드의 일반화가 가능하다고 한다
    
    그래서 추가한 코드가 있고 테스트가 실패하므로 빨간 막대

--- WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
+++ WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
@@ -37,6 +37,7 @@ class WyCashApplicationTests {
 	@Test
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 
 }
