2e1bb393a4472e0c479714f5fa7578b8026b40ee
syureu <tor01237@gmail.com>
2023-05-07 18:59:06 +0900

    주기의 마지막 : 중복을 제거하기 위해 리팩토링을 한다
    작은 단계를 발자
    필드에 바로 5 * 2 를 할당해주는걸 times 메서드로 옮겨줘도 테스트는 여전히 성공한다

--- WyCash/src/main/java/me/syureu/wycash/entity/Dollar.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Dollar.java
@@ -2,12 +2,13 @@ package me.syureu.wycash.entity;
 
 public class Dollar {
 
-	public int amount = 5 * 2;
+	public int amount;
 
 	public Dollar(int amount) {
 	}
 
 	public void times(int multiplier) {
+		amount = 5 * 2;
 	}
 
 }
