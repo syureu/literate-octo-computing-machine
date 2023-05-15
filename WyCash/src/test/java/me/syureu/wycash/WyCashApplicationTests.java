95fd2935d0a8d758e868f2216344f6ac0a39d789
syureu <tor01237@gmail.com>
2023-05-15 23:16:10 +0900

    5CHF * 2 = 10CHF
    프랑은 달러와 유사한 객체이므로 달러를 테스트하는 코드를 복붙하고
    달러 객체를 프랑 객체로 바꾼다
    
    프랑 객체는 없으므로 컴파일 에러로 인한 빨간 막대 상태이다

--- WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
+++ WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
@@ -41,4 +41,17 @@ class WyCashApplicationTests {
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+
+	@Test
+	public void testFrancEquality() {
+		assertTrue(new Franc(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Franc(6)));
+	}
+
 }
