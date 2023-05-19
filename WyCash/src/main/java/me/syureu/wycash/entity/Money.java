d9be73b5b51e2e8c33577703d23efccbf3df780a
syureu <tor01237@gmail.com>
2023-05-19 23:10:12 +0900

    Dollar/Franc 중복
    컴파일 에러를 해결하기 위해 팩토리 메서드를 추가해 주자

--- WyCash/src/main/java/me/syureu/wycash/entity/Money.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Money.java
@@ -3,6 +3,10 @@ package me.syureu.wycash.entity;
 public class Money {
 	protected int amount;
 
+	public static Dollar dollar(int amount) {
+		return new Dollar(amount);
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		return this.getClass().equals(obj.getClass()) && amount == ((Money)obj).amount;
