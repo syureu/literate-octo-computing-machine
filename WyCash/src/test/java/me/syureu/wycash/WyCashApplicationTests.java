fdd2a70ca7dc74b5ee3e25931deecf341c3cad8c
syureu <tor01237@gmail.com>
2023-05-19 23:12:02 +0900

    Dollar/Franc 중복
    우리의 큰 할일을 다시 상기해보면 Dollar, Franc를 없애는 것이 목표이므로
    테스트에서 Dollar의 참조를 제거하기 위해 Money로 변경하자
    
    이 경우 Money엔 times 메서드가 없으므로 다른 컴파일 에러가 발생한다
    여전히 빨간 막대 상태

--- WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
+++ WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
@@ -46,7 +46,7 @@ class WyCashApplicationTests {
 
 	@Test
 	public void testDollarMultiplication() {
-		Dollar five = Money.dollar(5);
+		Money five = Money.dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
