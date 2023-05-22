153dc653d394db64f95fa80ee91b3b36faf71652
syureu <tor01237@gmail.com>
2023-05-22 21:47:30 +0900

    통화?
    중복 제거를 위해 currency의 구현을 통일해보자
    currency 필드를 갖고있고, 생성자에서 해당 값을 할당해주자
    그러면 currency 메서드의 구현이 같아지므로 중복 제거를 할 수 있게 됐다

--- WyCash/src/main/java/me/syureu/wycash/entity/Dollar.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Dollar.java
@@ -2,12 +2,15 @@ package me.syureu.wycash.entity;
 
 public class Dollar extends Money {
 
+	private String currency;
+
 	public Dollar(int amount) {
 		this.amount = amount;
+		this.currency = "USD";
 	}
 
 	@Override public String currency() {
-		return "USD";
+		return this.currency;
 	}
 
 	public Money times(int multiplier) {
