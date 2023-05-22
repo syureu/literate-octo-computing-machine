27fa9247574d9c91e97eb97d75e7f988b701b265
syureu <tor01237@gmail.com>
2023-05-22 22:01:37 +0900

    통화?
    프랑에선 단계를 작게 가져가봤으므로
    같은 구조라고 할 수 있는 달러는 한번에 크게 변경해보자

--- WyCash/src/main/java/me/syureu/wycash/entity/Dollar.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Dollar.java
@@ -2,13 +2,13 @@ package me.syureu.wycash.entity;
 
 public class Dollar extends Money {
 
-	public Dollar(int amount) {
+	public Dollar(int amount, String currency) {
 		this.amount = amount;
-		this.currency = "USD";
+		this.currency = currency;
 	}
 
 	public Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
 	}
 
 }
