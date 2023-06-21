ca9c3fc3b8d4d3c424ad475ddc1857836a651282
syureu <tor01237@gmail.com>
2023-06-21 21:39:27 +0900

    $5 + $5 = $10
    책에서는 환율과 관련된 테스트를 작성하기 위한 긴 서사를 했지만
    간단하게 얘기하면
    다른 환율을 고려한 산술 연산에 대한 것을 표현하는 Money를 흉내낸 다른 객체를 만드는 방법이다
    이러한것을 만들기위해 sum을 reduced로 대체하고,
    이 reduced를 만들기위한 연산식을 하나하나 만들어 나간다

--- WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
+++ WyCash/src/test/java/me/syureu/wycash/WyCashApplicationTests.java
@@ -70,8 +70,8 @@ class WyCashApplicationTests {
 
 	@Test
 	void testSimpleAddition() {
-		Money sum = Money.dollar(5).plus(Money.dollar(5));
-		assertEquals(Money.dollar(10), sum);
+		...
+		assertEquals(Money.dollar(10), reduced);
 	}
 
 }
