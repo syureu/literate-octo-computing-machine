cd97971d0b826fa85cdb1e55491907b52dbf588a
syureu <tor01237@gmail.com>
2023-07-08 23:09:23 +0900

    Money에 대한 통화 변환을 수행하는 Reduce
    이제 Money가 환율을 Bank에 물어보게 함으로써 드러븐 환율내용을 몰라도 된다

--- WyCash/src/main/java/me/syureu/wycash/entity/Money.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Money.java
@@ -1,50 +1,50 @@
 package me.syureu.wycash.entity;
 
 public class Money implements Expression {
-	protected int amount;
-	protected String currency;
+    protected int amount;
+    protected String currency;
 
-	public Money(int amount, String currency) {
-		this.amount = amount;
-		this.currency = currency;
-	}
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
-	public static Money dollar(int amount) {
-		return new Money(amount, "USD");
-	}
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
 
-	public static Money franc(int amount) {
-		return new Money(amount, "CHF");
-	}
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		return amount == ((Money)obj).amount && currency().equals(((Money)obj).currency());
-	}
+    @Override
+    public boolean equals(Object obj) {
+        return amount == ((Money) obj).amount && currency().equals(((Money) obj).currency());
+    }
 
-	@Override
-	public String toString() {
-		return "Money{" +
-			"amount=" + amount +
-			", currency='" + currency + '\'' +
-			'}';
-	}
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 
-	public String currency() {
-		return this.currency;
-	}
+    public String currency() {
+        return this.currency;
+    }
 
-	public Money times(int multiplier) {
-		return new Money(amount * multiplier, currency);
-	}
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
-	public Expression plus(Money addend) {
-		return new Sum(this, addend);
-	}
+    public Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
 
-	@Override
-	public Money reduce(Bank bank, String to) {
-		int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
-		return new Money(amount / rate, to);
-	}
+    @Override
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
+    }
 }
