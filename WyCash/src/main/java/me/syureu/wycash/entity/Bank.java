0f89e35686f4e9555fcbdcce24ec9438a7564cc4
syureu <tor01237@gmail.com>
2023-07-08 23:06:47 +0900

    Money에 대한 통화 변환을 수행하는 Reduce
    환율을 Bank가 처리하게 하도록 하기 위해
    Expression.reduce의 인자로 Bank를 넘겨주자
    Expression.reduce의 시그니처도 Bank를 받을 수 있게 변경하고
    Sum과 Money의 오버라이드 메서드 시그니쳐도 변경해주자
    
    이제 Bank에 환율을 계산하도록 할 수 있고 이를 추가해주자

--- WyCash/src/main/java/me/syureu/wycash/entity/Bank.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Bank.java
@@ -1,8 +1,12 @@
 package me.syureu.wycash.entity;
 
 public class Bank {
-	public Money reduce(Expression source, String to) {
-		return source.reduce(to);
-	}
+    public Money reduce(Expression source, String to) {
+        return source.reduce(this, to);
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
 
 }
--- WyCash/src/main/java/me/syureu/wycash/entity/Expression.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Expression.java
@@ -1,5 +1,5 @@
 package me.syureu.wycash.entity;
 
 public interface Expression {
-	Money reduce(String to);
+	Money reduce(Bank bank, String to);
 }
