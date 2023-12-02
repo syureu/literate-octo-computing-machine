7e0b6b4299ed457e539d13cbae3906a4d3ec70c9
syureu <tor01237@gmail.com>
2023-12-02 19:51:19 +0900

    Money에 대한 통화 변환을 수행하는 Reduce
    Bank 가 환율에 대한 정보를 질의할 해시테이블이 필요하다.
    assertEquals를 사용해서 배열 두개를 비교하니 동치성 비교에서 실패하므로
    키를 위한 Pair 객체를 따로 만들고 equals, hashCode를 대충 override 해주자. (사실 ide가 잘 만들어주는데 이렇게 개떡같이 만들 필요가 있나 싶다)

--- WyCash/src/main/java/me/syureu/wycash/entity/Bank.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Bank.java
@@ -1,5 +1,7 @@
 package me.syureu.wycash.entity;
 
+import java.util.Objects;
+
 public class Bank {
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
@@ -9,4 +11,24 @@ public class Bank {
         return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 
+    private static class Pair {
+        private String from;
+        private String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            Pair pair = (Pair) o;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
 }
