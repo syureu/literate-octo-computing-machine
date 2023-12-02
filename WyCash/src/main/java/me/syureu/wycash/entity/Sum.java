fc716b2dee3f5e233844af1be7a05a8d8c4ab1f2
syureu <tor01237@gmail.com>
2023-12-02 20:22:22 +0900

    15. 서로 다른 통화 더하기 - $5 + 10CHF = $10(환율이 2:1일 경우)
    USD로의 reduce를 수행하고
    테스트는 초록 막대가 되었다

--- WyCash/src/main/java/me/syureu/wycash/entity/Sum.java
+++ WyCash/src/main/java/me/syureu/wycash/entity/Sum.java
@@ -1,17 +1,17 @@
 package me.syureu.wycash.entity;
 
 public class Sum implements Expression {
-	public Money augend;
-	public Money addend;
+    public Money augend;
+    public Money addend;
 
-	public Sum(Money augend, Money addend) {
-		this.augend = augend;
-		this.addend = addend;
-	}
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
 
-	public Money reduce(Bank bank, String to) {
-		int amount = augend.amount + addend.amount;
-		return new Money(amount, to);
-	}
+    public Money reduce(Bank bank, String to) {
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
+        return new Money(amount, to);
+    }
 
 }
